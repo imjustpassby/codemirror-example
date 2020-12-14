9f49e11486fab1a58f1419517b2cc87bd10e378f
imjustpassby <260809970@qq.com>
2020-12-14 16:40:59 +0800

    fix: request baseURL

--- src/api/codeMirror.js
+++ src/api/codeMirror.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function postSql(data) {
   return request({
-    url: '/api/postSql',
+    url: '/postSql',
     method: 'POST',
     data
   })
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
-
-const BASE_URL = 'http://localhost:9111'
+// 开发环境用于webpack-devServer的proxy，/api开头
+const DEV_BASE_URL = 'http://localhost:9111/api'
+// 生产环境后端真实地址
+const Production_BASE_URL = 'http://localhost:3000'
 const request = axios.create({
-  baseUrl: BASE_URL
+  baseURL:
+    process.env.NODE_ENV === 'production' ? Production_BASE_URL : DEV_BASE_URL
 })
 
 request.interceptors.request.use(config => {
